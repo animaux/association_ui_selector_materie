c2dd75680d2e1d29e85255b3930c3327ce587b84
Alexander Rutz <ar@animaux.de>
2017-01-31 09:28:39 +0100

    Init

---
+++ lang/lang.de.php
@@ -0,0 +1,25 @@
+<?php
+
+	$about = array(
+		'name' => 'Deutsch',
+		'author' => array(
+			'name' => 'Alexander Rutz',
+			'email' => 'alex@animaux.de',
+			'website' => 'http://animaux.de'
+		),
+		'release-date' => '2017-01-31'
+	);
+
+	$dictionary = array(
+
+    'Remove' => 
+		'×',
+		
+		'drag to reorder' =>
+		'zum Sortieren Einträge verschieben',
+		
+		'Search and select' => 
+		'suchen und auswählen'
+
+
+	);
